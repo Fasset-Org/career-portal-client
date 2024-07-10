0e60721add2dcbf318d96f584f7085f7376cf389
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-07-10 15:06:39 +0200

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import StudentsHome from '../components/StudentsHome'
+import React from "react";
+import StudentsHome from "../components/StudentsHome";
+import { useQuery } from "@tanstack/react-query";
+import ApiQueries from "../apiQuries";
+import AdminHome from "../components/admin/AdminHome";
 
 const Dashboard = () => {
-  return (
-    <StudentsHome />
-  )
-}
+  const { data } = useQuery({
+    queryKey: ["userInfo"],
+    queryFn: () => {
+      return ApiQueries.userInfo();
+    }
 
-export default Dashboard
+    // staleTime: 1000 * 60 * 60 * 24
+  });
+
+  console.log(data);
+  switch (data?.userType) {
+    case "admin":
+      return <AdminHome />;
+    case "student":
+      return <StudentsHome />;
+    default:
+      return <AdminHome />;
+  }
+};
+
+export default Dashboard;
