d7b89a6beb1372842451c8e37c85743d6b96c0f5
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-07-10 15:53:33 +0200

    Update AdminHome.jsx

--- src/components/admin/AdminHome.jsx
+++ src/components/admin/AdminHome.jsx
@@ -1,12 +1,8 @@
-import { Stack } from "@mui/material";
 import React from "react";
+import { Navigate } from "react-router-dom";
 
 const AdminHome = () => {
-  return (
-    <Stack padding={2} mt={8.5} border={3}>
-      <div>AdminHome</div>
-    </Stack>
-  );
+  return <Navigate to="/learners" />;
 };
 
 export default AdminHome;
