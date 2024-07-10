d28b869280796791e8af786df6514a02bce617b2
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-07-10 15:07:37 +0200

    Create AdminHome.jsx

---
+++ src/components/admin/AdminHome.jsx
@@ -0,0 +1,12 @@
+import { Stack } from "@mui/material";
+import React from "react";
+
+const AdminHome = () => {
+  return (
+    <Stack padding={2} mt={8.5} border={3}>
+      <div>AdminHome</div>
+    </Stack>
+  );
+};
+
+export default AdminHome;
