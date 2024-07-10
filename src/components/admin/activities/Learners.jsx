e870f70542b7ae9f529e0702eb06f19a56041373
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-07-10 15:53:41 +0200

    Create Learners.jsx

---
+++ src/components/admin/activities/Learners.jsx
@@ -0,0 +1,12 @@
+import { Stack } from '@mui/material'
+import React from 'react'
+
+const Learners = () => {
+  return (
+    <Stack padding={2} mt={8.5} minHeight='88vh'>
+      <div>Leaners</div>
+    </Stack>
+  )
+}
+
+export default Learners
