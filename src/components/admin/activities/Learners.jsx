0270ba147813ec4bae4cc158152f93da6c2823c1
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-08-30 12:46:35 +0200

    Update Learners.jsx

--- src/components/admin/activities/Learners.jsx
+++ src/components/admin/activities/Learners.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { DataGrid, GridToolbar } from "@mui/x-data-grid";
-import { LinearProgress, Stack, Typography } from "@mui/material";
+import { Box, LinearProgress, Stack, Typography } from "@mui/material";
 import { useQuery } from "@tanstack/react-query";
 import ApiQueries from "../../../apiQuries";
 
@@ -16,7 +16,7 @@ const Learners = () => {
     {
       field: "identificationNumber",
       headerName: "ID Number",
-      editable: true
+      editable: true,
     },
     {
       field: "passportNumber",
@@ -26,17 +26,18 @@ const Learners = () => {
     {
       field: "firstName",
       headerName: "First Name",
-      editable: true
+      editable: true,
     },
     {
       field: "lastName",
       headerName: "Last Name",
-      editable: true
+      editable: true,
     },
     {
       field: "email",
       headerName: "Email",
-      editable: true
+      editable: true,
+      width: 200
     },
     {
       field: "race",
@@ -62,7 +63,6 @@ const Learners = () => {
       field: "basicEducation",
       headerName: "Basic Education",
       editable: true,
-      width: 300,
       renderCell: (params) => {
         return (
           <Stack height="100%" justifyContent="center">
@@ -70,6 +70,86 @@ const Learners = () => {
               {params.row.basicEducation}
             </Typography>
           </Stack>
+        );
+      }
+    },
+    {
+      field: "tertiaryEducation",
+      headerName: "Teriary Education",
+      editable: true,
+      width: 300,
+      renderCell: (params) => {
+        const tertiaryEducations = params.row.tertiaryEducation?.split(",");
+
+        return (
+          <Stack height="100%" justifyContent="center">
+            <Typography sx={{ textWrap: "wrap", fontSize: 11 }}>
+              {tertiaryEducations?.map((education) => {
+                return (
+                  <span>
+                    &#9642; {education}
+                    <br />
+                  </span>
+                );
+              })}
+            </Typography>
+          </Stack>
+        );
+      }
+    },
+    {
+      field: "studentProgrammes",
+      headerName: "Learner Interests",
+      editable: true,
+      width: 300,
+      renderCell: (params) => {
+        const studentProgrammes = params.row.studentProgrammes?.split(",");
+
+        return (
+          <Stack height="100%" justifyContent="center">
+            <Typography sx={{ textWrap: "wrap", fontSize: 11 }}>
+              {studentProgrammes?.map((programme) => {
+                return (
+                  <span>
+                    &#9642; {programme}
+                    <br />
+                  </span>
+                );
+              })}
+            </Typography>
+          </Stack>
+        );
+      }
+    },
+
+    {
+      field: "profileProgress",
+      headerName: "Profile Completion",
+      editable: true,
+      width: 200,
+      renderCell: (params) => {
+        return (
+          <Box display="flex" alignItems="center" width="100%">
+            <Box width="100%" mr={1}>
+              <LinearProgress
+                variant="determinate"
+                value={params?.row?.profileProgress}
+                color={
+                  params?.row?.profileProgress < 50
+                    ? "error"
+                    : params?.row?.profileProgress >= 50 &&
+                      params?.row?.profileProgress < 75
+                    ? "warning"
+                    : "success"
+                }
+              />
+            </Box>
+            <Box minWidth={35}>
+              <Typography variant="body2" color="textSecondary">{`${Math.round(
+                params?.row?.profileProgress
+              )}%`}</Typography>
+            </Box>
+          </Box>
         );
       }
     }
@@ -87,7 +167,17 @@ const Learners = () => {
               Object.values(student?.studentAddress).join(" "),
             basicEducation:
               student?.basicEducation &&
-              Object.values(student?.basicEducation).join(" ")
+              Object.values(student?.basicEducation).join(" "),
+            tertiaryEducation:
+              student?.tertiaryEducation &&
+              student?.tertiaryEducation
+                .map((edu) => Object.values(edu).join(" "))
+                .join(", "),
+            studentProgrammes:
+              student?.studentProgrammes &&
+              student?.studentProgrammes
+                .map((programe) => Object.values(programe.programmes).join(" "))
+                .join(", ")
           };
         })
       );
@@ -110,7 +200,7 @@ const Learners = () => {
           rows={rows.length > 0 && rows}
           columns={columns}
           autoHeight
-          getRowHeight={() => Math.max(50, 'auto')}
+          getRowHeight={() => "auto"}
           autoHeightMax={400}
           initialState={{
             pagination: {
@@ -129,6 +219,18 @@ const Learners = () => {
           slotProps={{
             toolbar: {
               showQuickFilter: true
+            }
+          }}
+          sx={{
+            "& .MuiDataGrid-columnHeaders": {
+              // backgroundColor: "#163683", // Header background color
+              fontWeight: "bolder", // Bold font
+              fontSize: 13
+            },
+            "& .MuiDataGrid-cell": {
+              display: "flex",
+              alignItems: "center" // Vertically align row content
+              // justifyContent: 'center', // Optional: horizontally center content
             }
           }}
         />
